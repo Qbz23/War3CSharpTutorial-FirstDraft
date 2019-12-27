c7a65bc839e83b228248fcadef5339f16daca3ab
Qbz <conorlavelleqbz@gmail.com>
2019-12-27 11:45:55 -0800

    Added DebugPrint helper

--- 1-SetUpEnviornment/War3Map.Template/War3Map.Template.Source/Program.cs
+++ 1-SetUpEnviornment/War3Map.Template/War3Map.Template.Source/Program.cs
@@ -2,11 +2,19 @@
 
 namespace War3Map.Template.Source
 {
+    internal class Helpers
+    { 
+        public static void DebugPrint(string s)
+        {
+            DisplayTextToPlayer(GetLocalPlayer(), 0, 0, s);
+        }
+    }
+
     internal static class Program
     {
         private static void Main()
         {
-            DisplayTextToPlayer(GetLocalPlayer(), 0, 0, "Hello World!");
+            Helpers.DebugPrint("Hello War3 C#!");
         }
     }
 }
