584578c00f5c75d192cf360b30145a33f2bdc153
Qbz <conorlavelleqbz@gmail.com>
2019-12-27 11:06:24 -0800

    Added template and gitignore for it

---
+++ 1-SetUpEnviornment/War3Map.Template/War3Map.Template.Launcher/Program.cs
@@ -0,0 +1,44 @@
+﻿using System.Diagnostics;
+using System.IO;
+
+using War3Net.Build;
+
+namespace War3Map.Template.Launcher
+{
+    internal static class Program
+    {
+        // Input
+        private const string SourceCodeProjectFolderPath = @"..\..\..\..\War3Map.Template.Source";
+        private const string AssetsFolderPath = @".\Assets\";
+
+        // Output
+        private const string OutputFolderPath = @"..\..\..\..\artifacts";
+        private const string OutputMapName = @"Testmap.w3x";asd
+
+        // Warcraft III
+        private const string Warcraft3ExecutableFilePath = null;
+        private const string Warcraft3CommandLineArgs = @"-nowfpause -graphicsapi Direct3D9 ";
+
+        private static void Main()
+        {
+            // Build and launch
+            var mapBuilder = new MapBuilder(OutputMapName);
+            if (mapBuilder.Build(CompilerOptions.GetCompilerOptions(SourceCodeProjectFolderPath, OutputFolderPath), AssetsFolderPath))
+            {
+                var mapPath = Path.Combine(OutputFolderPath, OutputMapName);
+                var absoluteMapPath = new FileInfo(mapPath).FullName;
+
+#if DEBUG
+                if (Warcraft3ExecutableFilePath != null)
+                {
+                    Process.Start(Warcraft3ExecutableFilePath, $"{Warcraft3CommandLineArgs} -loadfile \"{absoluteMapPath}\"");
+                }
+                else
+#endif
+                {
+                    Process.Start("explorer.exe", $"/select, \"{absoluteMapPath}\"");
+                }
+            }
+        }
+    }
+}
