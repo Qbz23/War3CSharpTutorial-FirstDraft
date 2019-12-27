79055905474adeef3ec377c2127dae05cc094d89
Qbz <conorlavelleqbz@gmail.com>
2019-12-27 13:35:40 -0800

    Set a patch of terrain to be grassy

--- 2-Terrain/War3Map.Template/War3Map.Template.Source/Program.cs
+++ 2-Terrain/War3Map.Template/War3Map.Template.Source/Program.cs
@@ -8,12 +8,23 @@ namespace War3Map.Template.Source
         {
             DisplayTextToPlayer(GetLocalPlayer(), 0, 0, s);
         }
+
+        public static int GetId(string s)
+        {
+            return (s[0] << 24) | (s[1] << 16) | (s[2] << 8) | (s[3]);
+        }
     }
 
     internal static class Program
     {
         private static void Main()
         {
+            FogEnable(false);
+            FogMaskEnable(false);
+
+            int grassId = Helpers.GetId("Lgrs");
+            SetTerrainType(0, 0, grassId, 0, 3, 0);
+
             Helpers.DebugPrint("Hello War3 C#!");
         }
     }
