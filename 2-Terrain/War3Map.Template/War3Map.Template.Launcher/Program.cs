a42385af683f27fa25b2456d6c1306e786c75b54
Qbz <conorlavelleqbz@gmail.com>
2019-12-27 12:16:56 -0800

    Copied 1 into new folder for 2 and updated gitignore

---
+++ 2-Terrain/War3Map.Template/War3Map.Template.Launcher/Program.cs
@@ -0,0 +1,44 @@
+﻿using System.Diagnostics;
+using System.IO;
+
+using War3Net.Build;
+
+namespace War3Map.Template.Launcher
+{
+    internal static class Program
+    {
+        // Input
+        private const string SourceCodeProjectFolderPath = @"..\..\..\..\War3Map.Template.Source";
+        private const string AssetsFolderPath = @".\Assets\";
+
+        // Output
+        private const string OutputFolderPath = @"..\..\..\..\artifacts";
+        private const string OutputMapName = @"War3CSharpTutorial.w3x";
+
+        // Warcraft III
+        private const string Warcraft3ExecutableFilePath = "C:\\Program Files (x86)\\Warcraft III Beta\\x86_64\\Warcraft III.exe";
+        private const string Warcraft3CommandLineArgs = @"-nowfpause -launch ";
+
+        private static void Main()
+        {
+            // Build and launch
+            var mapBuilder = new MapBuilder(OutputMapName);
+            if (mapBuilder.Build(CompilerOptions.GetCompilerOptions(SourceCodeProjectFolderPath, OutputFolderPath), AssetsFolderPath))
+            {
+                var mapPath = Path.Combine(OutputFolderPath, OutputMapName);
+                var absoluteMapPath = new FileInfo(mapPath).FullName;
+
+#if DEBUG
+                if (Warcraft3ExecutableFilePath != null)
+                {
+                    Process.Start(Warcraft3ExecutableFilePath, $"{Warcraft3CommandLineArgs} -loadfile \"{absoluteMapPath}\"");
+                }
+                else
+#endif
+                {
+                    Process.Start("explorer.exe", $"/select, \"{absoluteMapPath}\"");
+                }
+            }
+        }
+    }
+}
---
+++ 2-Terrain/War3Map.Template/War3Map.Template.Source/Program.cs
@@ -0,0 +1,20 @@
+﻿using static War3Api.Common;
+
+namespace War3Map.Template.Source
+{
+    internal class Helpers
+    { 
+        public static void DebugPrint(string s)
+        {
+            DisplayTextToPlayer(GetLocalPlayer(), 0, 0, s);
+        }
+    }
+
+    internal static class Program
+    {
+        private static void Main()
+        {
+            Helpers.DebugPrint("Hello War3 C#!");
+        }
+    }
+}
