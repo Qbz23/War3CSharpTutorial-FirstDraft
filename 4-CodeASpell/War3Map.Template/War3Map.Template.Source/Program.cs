ef1b38d076d3a36c483d319a214843aefabc8da9
Qbz <conorlavelleqbz@gmail.com>
2019-12-28 18:09:50 -0800

    Spawn units to hit

--- 4-CodeASpell/War3Map.Template/War3Map.Template.Source/Program.cs
+++ 4-CodeASpell/War3Map.Template/War3Map.Template.Source/Program.cs
@@ -1,4 +1,5 @@
 ﻿using static War3Api.Common;
+using System;
 
 namespace War3Map.Template.Source
 {
@@ -43,6 +44,30 @@ namespace War3Map.Template.Source
             TriggerRegisterPlayerUnitEvent(spellTrigger, GetLocalPlayer(), EVENT_PLAYER_UNIT_SPELL_EFFECT, null);
             TriggerAddCondition(spellTrigger, Condition(spellCondition));
             TriggerAddAction(spellTrigger, spellActions);
+
+            // The player that will own our spawned units, 26 is neutral hostile 
+            player neutralHostile = Player(26);
+            // How many wisps to spawn 
+            int numWisps = 12;
+            // The radius of the cirlce to spawn wisps in 
+            float circleRadius = 500.0f;
+            // the angle between each wisp 
+            float angleDelta = (2.0f * 3.1415f) / numWisps;
+            // for each wisp we want to spawn...
+            for (uint i = 0; i < numWisps; ++i)
+            {
+                // Calculate their position in the circle
+                float x = circleRadius * (float)Math.Cos(angleDelta * i);
+                float y = circleRadius * (float)Math.Sin(angleDelta * i);
+
+                // Save that position as a location 
+                location wispSpawnLoc = Location(x, y);
+                // Spawn the wisp - It's easier to use ByName if you want to spawn 
+                // a built-in unit compared to a custom unit 
+                CreateUnitAtLocByName(neutralHostile, "Wisp", wispSpawnLoc, 0.0f);
+                // Clean up location now that we no longer need it 
+                RemoveLocation(wispSpawnLoc);
+            }
 
             //int grassId = Helpers.GetId("Lgrs");
             //SetTerrainType(0, 0, grassId, 0, 3, 0);
